de995292f72895ecd257c55ec5b8c215074ee92d
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-09-15 22:23:45 +0100

    Moviecard hover

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
+
+// Import FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
+// import required modules
+import { EffectCoverflow, Pagination, Zoom } from "swiper/modules";
 
 import "../styles/MovieCard.css";
 
-// import required modules
-import { EffectCoverflow, Pagination } from "swiper/modules";
-
 export default function MovieCard() {
+  const [favorite, setFavorite] = useState([]);
   const [movies, setMovies] = useState([]);
   const apiUrl =
-    "https://api.themoviedb.org/3/movie/popular?api_key=f88a88c56f80363e3e2757e7a1a6f5a0&page=1";
+    "https://api.themoviedb.org/3/movie/popular?api_key=f88a88c56f80363e3e2757e7a1a6f5a0&page=3&limit=10";
 
   useEffect(() => {
     fetch(apiUrl)
       .then((response) => response.json())
       .then((data) => setMovies(data.results));
   }, []);
+
+  function addFavorites() {
+    setFavorite(!favorite);
+  }
 
   return (
     <div className="MovieCard">
@@ -31,21 +40,31 @@ export default function MovieCard() {
         centeredSlides={true}
         slidesPerView={"auto"}
         coverflowEffect={{
-          rotate: 50,
+          rotate: 20,
           stretch: 0,
-          depth: 100,
+          depth: 350,
           modifier: 1,
           slideShadows: true,
         }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
+        loop={true}
+        modules={[EffectCoverflow, Pagination, Zoom]}
         className="mySwiper"
       >
         {movies.map((movie, index) => {
           const apiImg = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`;
+
           return (
             <SwiperSlide key={index}>
               <img src={apiImg} alt="Movie poster" />
+              <div className="info">
+                <span>
+                  <p className="title">{movie.title}</p>
+                  <p className="vote">{movie.vote_average}</p>
+                </span>
+                <div>
+                  <FontAwesomeIcon icon={regularHeart} onClick={addFavorites} />
+                </div>
+              </div>
             </SwiperSlide>
           );
         })}
