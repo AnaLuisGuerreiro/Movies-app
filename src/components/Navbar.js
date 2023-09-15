9751bfee5db5fbbda96a5335d65ba411896bbc23
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-09-15 22:23:06 +0100

    Change navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,60 @@
 import React from "react";
-import btn from "../img/btn-home_logo.png";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faThumbsUp,
-  faThumbsDown,
-  faListUl,
-  faHouse,
-} from "@fortawesome/free-solid-svg-icons";
 
 import "../styles/Navbar.css";
 
 export default function Navbar() {
   return (
     <div className="Navbar">
-      <div className="float">
-        <a href="www.facebook.com">
-          <img src={btn} alt="Home/logo" />
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">
+          Navbar
         </a>
-      </div>
-      <div className="box">
-        <div className="row">
-          <div className="col">
-            <FontAwesomeIcon icon={faHouse} className="icon" />
-          </div>
-          <div className="col">
-            <FontAwesomeIcon icon={faThumbsDown} className="icon" />
-          </div>
-          <div className="col">
-            <FontAwesomeIcon icon={faThumbsUp} className="icon" />
-          </div>
-          <div className="col">
-            <FontAwesomeIcon icon={faListUl} className="icon" />
-          </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="/">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/">
+                Link
+              </a>
+            </li>
+            <li class="nav-item dropdown">
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="/">
+                  Action
+                </a>
+                <a class="dropdown-item" href="/">
+                  Another action
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="/">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="/">
+                Disabled
+              </a>
+            </li>
+          </ul>
         </div>
-      </div>
+      </nav>
     </div>
   );
 }
