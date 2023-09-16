2bb51867f6eae38f7b1254424e2a6f258c09cf13
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-09-17 00:09:15 +0100

    Optimize navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,27 @@
 import React from "react";
 import btn from "../img/btn-home_logo.png";
 
+import { Link } from "react-router-dom";
+
+import Search from "./Search";
 import "../styles/Navbar.css";
 
 export default function Navbar() {
   return (
     <div className="Navbar">
-      <nav class="navbar navbar-expand-lg navbar-light ">
-        <a class="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-lg">
+        <Link to="/" className="navbar-brand" href="/">
           <img src={btn} alt="logo" />
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+        </Link>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/">
-                Link
-              </a>
-            </li>
-            <li class="nav-item dropdown">
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="/">
-                  Action
-                </a>
-                <a class="dropdown-item" href="/">
-                  Another action
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="/">
-                  Something else here
-                </a>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="/">
-                Disabled
-              </a>
-            </li>
-          </ul>
-        </div>
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to="/Favorites" className="fav" href="/">
+              Favorites
+            </Link>
+          </li>
+        </ul>
+        <Search />
       </nav>
     </div>
   );
