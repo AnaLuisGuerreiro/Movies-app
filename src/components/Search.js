286706276ab5d873f066070c7e77ba2b6da7a973
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-09-17 00:08:46 +0100

    Start form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "../styles/Search.css";
 
 export default function Search() {
-  return <div className="Search"></div>;
+  return (
+    <div className="Search">
+      <form>
+        <input type="text" placeholder=" Type a movie" />
+      </form>
+    </div>
+  );
 }
