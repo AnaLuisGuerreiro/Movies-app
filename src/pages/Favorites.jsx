2e1032bba54fb59cde9a092b99f0fd95cce1d269
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-09-21 20:08:14 +0100

    Favorites component and styles

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useFavorites } from "../components/global/FavoritesContext";
+import "../styles/Favorites.css";
 
 const Favorites = () => {
+  const { favorites } = useFavorites();
+
   return (
     <div className="Favorites">
-      <span>Favorites</span>
+      <h2>Favorites</h2>
+      <div className="info">
+        {favorites.map((movie) => (
+          <div key={movie.id} className="cards">
+            <img
+              src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+              alt={movie.title}
+              className="images"
+            />
+            <h6 className="titles">{movie.title}</h6>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
