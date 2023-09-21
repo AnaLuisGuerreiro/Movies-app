21c34393b9ff22e68a8d46efccff945abd32c2b8
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-09-21 20:11:14 +0100

    All the conditions for the favorite future

--- src/components/LoveButton.jsx
+++ src/components/LoveButton.jsx
@@ -1,25 +1,32 @@
-import React, { useState } from "react";
-import "../styles/LoveButton.css";
-// Import FontAwesome
+import React from "react";
+import { useFavorites } from "./global/FavoritesContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart as regularHeart } from "@fortawesome/free-regular-svg-icons";
 import { faHeart as solidHeart } from "@fortawesome/free-solid-svg-icons";
+import "../styles/LoveButton.css";
 
-export default function LoveButton() {
-  const [love, setLove] = useState("false");
+export default function LoveButton({ movie }) {
+  const { favorites, addToFavorites, removeFromFavorites } = useFavorites();
+  console.log(movie);
+  const isFavorite = favorites.some((favMovie) => favMovie.id === movie.id);
 
   function toggleLove() {
-    setLove(!love);
+    if (isFavorite) {
+      removeFromFavorites(movie.id);
+    } else {
+      addToFavorites(movie);
+    }
   }
 
   return (
     <div className="LoveButton">
       <div onClick={toggleLove}>
-        <FontAwesomeIcon
-          icon={love ? regularHeart : solidHeart}
-          className="heart"
-        />
-        <span>Add favorites</span>
+        {isFavorite ? (
+          <FontAwesomeIcon icon={solidHeart} className="heart" />
+        ) : (
+          <FontAwesomeIcon icon={regularHeart} className="heart" />
+        )}
+        <span>Add to favorites</span>
       </div>
     </div>
   );
