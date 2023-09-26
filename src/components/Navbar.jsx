23c163f76189f6076c531ae6ff03c5354d3fa05b
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-09-26 15:11:24 +0100

    Deal fetch errors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,26 +15,27 @@ export default function Navbar() {
     setQuery(newQuery);
 
     if (newQuery.trim() !== "") {
-      // Redirection to MovieCards page with API results as part of the route
-      navigate("/MovieCards", { state: { query: newQuery } });
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=f88a88c56f80363e3e2757e7a1a6f5a0&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data.erros && data.results.length > 0) {
+            // Redirection to MovieCards page with API results as part of the route
+            navigate("/MovieCards", { state: { results: data.results } });
+          } else {
+            // Redirect to the home page if there are no valid results
+            navigate("/MovieCards");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          // Handle any fetch errors here
+        });
     } else {
       // Redirect to the home page if the search query is empty
       navigate("/");
     }
-
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=f88a88c56f80363e3e2757e7a1a6f5a0&language=en-US&page=1&include_adult=false&query=${e.target.value}`
-    ).then((res) =>
-      res.json().then((data) => {
-        if (!data.erros) {
-          // Redirection to MovieCards page with API results as part of the route
-          navigate("/MovieCards", { state: { results: data.results } });
-        } else {
-          // Redirect to the home page if the search query is empty
-          navigate("/MovieCards");
-        }
-      })
-    );
   }
 
   return (
