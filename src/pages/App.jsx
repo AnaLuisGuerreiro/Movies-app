b4c5c19e4bb484b4411ae2cb3bb6a926846539f9
AnaLuisGuerreiro <124161000+AnaLuisGuerreiro@users.noreply.github.com>
2023-10-06 18:15:21 +0100

    footer and updates

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Importe Routes em vez de Switch
 
+// FontAwesome Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import MovieCarousel from "../components/MovieCarousel";
@@ -28,6 +33,26 @@ function App() {
             </Router>
           </div>
         </div>
+        <footer className="Footer">
+          <span className="coded"> Coded by Ana Guerreiro </span>
+          <br />
+          <a href="https://github.com/AnaLuisGuerreiro/weather-react">
+            <FontAwesomeIcon
+              icon={faGithub}
+              style={{ color: "#51c9bd" }}
+              target="_blank"
+              className="git"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/ana-luis-guerreiro/">
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              style={{ color: "#2767a3" }}
+              target="_blank"
+              className="linkedin"
+            />
+          </a>
+        </footer>
       </div>
     </FavoritesProvider>
   );
